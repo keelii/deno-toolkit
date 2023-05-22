d630f4ed2e76c2247c747e31b0c9c8817c2907c5
bjzhouqili <zhouqili@jd.com>
2023-05-22 13:19:08 +0800

    add tests.

---
+++ src/main_test.ts
@@ -0,0 +1,7 @@
+import { assertEquals } from "https://deno.land/std@0.188.0/testing/asserts.ts";
+
+Deno.test("my test", async (t) => {
+  await t.step("step_1", async (t) => {
+    assertEquals(1, 1);
+  });
+});
