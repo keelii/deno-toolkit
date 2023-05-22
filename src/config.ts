2d8fbd3c90e2b472c09a24178a04cd5e0af04774
bjzhouqili <zhouqili@jd.com>
2023-05-22 14:06:04 +0800

    convert to number.

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const PORT = Deno.env.get("DENO_PORT") || 4000;
+export const PORT = Deno.env.has("DENO_PORT") ? Number(Deno.env.get("DENO_PORT")) : 4000;
 export const ENV = Deno.env.get("DENO_ENV") || "prd";
 export const IS_DEV = ENV === "dev";
 export const CORS_ALLOW_ORIGIN = "*";
